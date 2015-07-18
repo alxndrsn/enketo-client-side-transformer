da003234d58592b0f03ecd24954c2fe02ac139e8
Alex Anderson <anderson@medicmobile.org>
2015-07-18 22:27:52 +0200

    Add framework for generated XML comparison

--- test/enketo-client-side-transformer.spec.js
+++ test/enketo-client-side-transformer.spec.js
@@ -4,8 +4,25 @@ describe('enketo-client-side-transformer spec', function() {
   it('should allow assertions', function() {
     assert.ok(true);
   });
+
+  it('should have acccess to log4js', function() {
+    assert.ok(log4js);
+  });
+
+  it('should launch the browser and output some logging', function() {
+    console.log('[console.log()] Testing 1, 2, 3...');
+    console.error('[console.error()] Testing 1, 2, 3...');
+  });
 });
 
 describe('enketo-client-side-transformer', function() {
-  // TODO
+  it('should be defined', function() {
+    assert.ok(enketo_client_side_transformer);
+  });
+
+  describe('#transform()', function() {
+    it('should be available', function() {
+      assert.typeOf(enketo_client_side_transformer.transform, 'function');
+    });
+  });
 });
