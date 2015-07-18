76019448aa183289cf5831770dfc79e1caaaabdc
Alex Anderson <anderson@medicmobile.org>
2015-07-18 16:40:10 +0200

    Put test framework in place.

---
+++ README.md
@@ -0,0 +1,4 @@
+enketo-client-side-transformer
+==============================
+
+Adaptation of enketo-transformer to work on popular browsers.
---
+++ test/enketo-client-side-transformer.spec.js
@@ -0,0 +1,11 @@
+var assert = chai.assert;
+
+describe('enketo-client-side-transformer spec', function() {
+  it('should allow assertions', function() {
+    assert.ok(true);
+  });
+});
+
+describe('enketo-client-side-transformer', function() {
+  // TODO
+});
