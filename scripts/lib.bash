364f75ff65ac2794364eb50367f77cc8d541204e
Alex Anderson <anderson@medicmobile.org>
2015-07-27 16:59:27 +0200

    Clean up xml-diff output

--- scripts/lib.bash
+++ scripts/lib.bash
@@ -2,13 +2,21 @@ log() {
 	echo "[$0] $@"
 }
 
+cmp_xml() {
+	cmp <(xmllint --format $left) <(xmllint --format $right)
+}
+
 diff_xml() {
 	local left="$1"
 	local right="$2"
-	log "====="
-	log "= $left vs $right"
-	# diff generated output with enketo-transformer output
-	diff <(xmllint --format $left) <(xmllint --format $right)
-	log "= $left vs $right"
-	log "====="
+	if cmp <(xmllint --format $left) <(xmllint --format $right) >/dev/null; then
+		log "=== [IDENTICAL] $left vs $right"
+	else
+		log "====="
+		log "=== [DIFFERENT] $left vs $right"
+		# diff generated output with enketo-transformer output
+		diff <(xmllint --format $left) <(xmllint --format $right)
+		log "= $left vs $right"
+		log "====="
+	fi
 }
